5c85e734968713901093a54ac8fa4c4fab7556da
Michael Moser <Michael.Moser11@web.de>
2026-08-24 16:52:40 +0200

    fix: typo

--- .devcontainer/scripts/on_start.sh
+++ .devcontainer/scripts/on_start.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 ## Clone newest version of dotfiles
 git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" pull --ff-only ||
-  echo "setup.sh: could not update dotfiles, continuing with the ones baked into the image" >&2
+  echo "on_start.sh: could not update dotfiles, continuing with the ones baked into the image" >&2
